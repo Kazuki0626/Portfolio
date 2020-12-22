c7c813ac0fe54c56947e9f44dab40994976bd49b
Kazuki0626 <kanchan210626@gmail.com>
2020-12-22 12:00:19 +0900

    ヘッダーを作りました

---
+++ script.js
@@ -0,0 +1,12 @@
+// TOP PAGE START
+$(function() {
+	setTimeout(function(){
+		$('.top-page-title h1').fadeIn(2000);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.top-page-title').fadeOut(1500);
+	},3000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
+
+
+// TOP PAGE END
