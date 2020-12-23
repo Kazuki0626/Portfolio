e0caa684c60851a08f90871e08becfdd22f3b616
Kazuki0626 <kanchan210626@gmail.com>
2020-12-23 16:59:14 +0900

    ヘッダーを再修正しました

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // TOP PAGE START
-$(function() {
-	setTimeout(function(){
-		$('.top-page-title h1').fadeIn(2000);
-	},500); //0.5秒後にロゴをフェードイン!
-	setTimeout(function(){
-		$('.top-page-title').fadeOut(1500);
-	},3000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+var startPos = 0,winScrollTop = 0;
+$(window).on('scroll',function(){
+    winScrollTop = $(this).scrollTop();
+    if (winScrollTop >= 500) {
+        $('.page-top').addClass('hide');
+    } else {
+        $('.page-top').removeClass('hide');
+    }
+    startPos = winScrollTop;
 });
 
+
+$(function() {
+	setTimeout(function(){
+		$('.page-top-title h1').fadeOut(1800);
+	},2000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
 
 // TOP PAGE END
