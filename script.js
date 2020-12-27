2ca1a7b3ac46317b6d3a3a226efe51c666980e7e
Kazuki0626 <kanchan210626@gmail.com>
2020-12-27 12:32:10 +0900

    途中経過までです

--- script.js
+++ script.js
@@ -3,17 +3,65 @@ var startPos = 0,winScrollTop = 0;
 $(window).on('scroll',function(){
     winScrollTop = $(this).scrollTop();
     if (winScrollTop >= 500) {
-        $('.page-top').addClass('hide');
+        $('.page-top-main').addClass('hide');
     } else {
-        $('.page-top').removeClass('hide');
+        $('.page-top-main').removeClass('hide');
     }
     startPos = winScrollTop;
 });
 
 $(function() {
 	setTimeout(function(){
-		$('.page-top-title h1').fadeOut(1800);
-	},2000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+		$('.page-top-main_title').fadeOut(1800);
+	},2000); 
 });
 
 // TOP PAGE END
+
+// SECOND=SECTION START
+
+$('.second-section_main_title, .second-section_sub_title_list').css('visibility','hidden');
+$(window).scroll(function(){
+var windowHeight = $(window).height(),
+    topWindow = $(window).scrollTop();
+$('.second-section_main_title, .second-section_sub_title_list').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+  $(this).addClass("second-section_fadeInDown");
+  }
+});
+});
+
+// SECOND SECTION END
+
+// VIEW SECTION START
+
+$('.view-first-left, .experience-section-sub, .view-second-right, .view-third-left, .view-section_title, .view-first-right, .first-description, .view-second_left, .second-description, .view-third-right, .third-description, .view-third-sub').css('visibility','hidden');
+$(window).scroll(function(){
+var windowHeight = $(window).height(),
+    topWindow = $(window).scrollTop();
+$('.view-first-left, .experience-section-sub, .view-second-right, .view-third-left, .view-section_title, .view-first-right, .first-description, .view-second_left, .second-description, .view-third-right, .third-description, .view-third-sub').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+  $(this).addClass("view-section_fadeInDown");
+  }
+});
+});
+
+// VIEW SECTION END
+
+// EXPERIENCE SECTION START
+
+$('.experience-section-main, .experience-first-left, .experience-first-right, .experience-second-left, .experience-second-right, .experience-third-left, .experience-third-right').css('visibility','hidden');
+$(window).scroll(function(){
+var windowHeight = $(window).height(),
+    topWindow = $(window).scrollTop();
+$('.experience-section-main, .experience-first-left, .experience-first-right, .experience-second-left, .experience-second-right, .experience-third-left, .experience-third-right').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 150){
+  $(this).addClass("experience-section_fadeInDown");
+  }
+});
+});
+
+// EXPERIENCE SECTION END
