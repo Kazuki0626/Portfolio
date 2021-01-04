a9eaf061592cef6fa7026a11b1751c0f1f6aa13f
Kazuki0626 <kanchan210626@gmail.com>
2021-01-05 00:25:53 +0900

    修正しました

--- script.js
+++ script.js
@@ -40,11 +40,11 @@ $('.second-section_main_title, .second-section_sub_title_list').each(function(){
 
 // VIEW SECTION START
 
-$('.view-first-left, .experience-section-sub, .view-second-right, .view-third-left, .view-section_title, .view-first-right, .first-description, .view-second_left, .second-description, .view-third-right, .third-description, .view-third-sub').css('visibility','hidden');
+$('.view-first_left, .experience-section_sub, .view-second_right, .view-third_left, .view-section_title, .view-first_right, .first-description, .view-second_left, .second-description, .view-third_right, .third-description, .view-third_sub').css('visibility','hidden');
 $(window).scroll(function(){
 var windowHeight = $(window).height(),
     topWindow = $(window).scrollTop();
-$('.view-first-left, .experience-section-sub, .view-second-right, .view-third-left, .view-section_title, .view-first-right, .first-description, .view-second_left, .second-description, .view-third-right, .third-description, .view-third-sub').each(function(){
+$('.view-first_left, .experience-section_sub, .view-second_right, .view-third_left, .view-section_title, .view-first_right, .first-description, .view-second_left, .second-description, .view-third_right, .third-description, .view-third_sub').each(function(){
   var targetPosition = $(this).offset().top;
   if(topWindow > targetPosition - windowHeight + 100){
   $(this).addClass("view-section_fadeInDown");
@@ -56,11 +56,11 @@ $('.view-first-left, .experience-section-sub, .view-second-right, .view-third-le
 
 // EXPERIENCE SECTION START
 
-$('.experience-section-main, .experience-first-left, .experience-first-right, .experience-second-left, .experience-second-right, .experience-third-left, .experience-third-right').css('visibility','hidden');
+$('.experience-section_main, .experience-first_left, .experience-first_right, .experience-second_left, .experience-second_right, .experience-third_left, .experience-third_right').css('visibility','hidden');
 $(window).scroll(function(){
 var windowHeight = $(window).height(),
     topWindow = $(window).scrollTop();
-$('.experience-section-main, .experience-first-left, .experience-first-right, .experience-second-left, .experience-second-right, .experience-third-left, .experience-third-right').each(function(){
+$('.experience-section_main, .experience-first_left, .experience-first_right, .experience-second_left, .experience-second_right, .experience-third_left, .experience-third_right').each(function(){
   var targetPosition = $(this).offset().top;
   if(topWindow > targetPosition - windowHeight + 150){
   $(this).addClass("experience-section_fadeInDown");
@@ -72,14 +72,14 @@ $('.experience-section-main, .experience-first-left, .experience-first-right, .e
 
 // ENJOYMENT SECTION START
 
-$('.enjoyment-section_main_layout, .enjoyment-section_sub, .enjoyment-section_first_left, .enjoyment-section_first_right, .enjoyment-section_second_left, .enjoyment-section_second_right, .enjoyment-section_third_left, .enjoyment-section_third_right, .enjoyment-section_fourth_left, .enjoyment-section_fourth_right').css('visibility','hidden');
+$('.enjoyment-section_main, .enjoyment-section_sub, .enjoyment-section_first_left, .enjoyment-section_first_right, .enjoyment-section_second_left, .enjoyment-section_second_right, .enjoyment-section_third_left, .enjoyment-section_third_right, .enjoyment-section_fourth_left, .enjoyment-section_fourth_right').css('visibility','hidden');
 $(window).scroll(function(){
 var windowHeight = $(window).height(),
     topWindow = $(window).scrollTop();
-$('.enjoyment-section_main_layout, .enjoyment-section_sub, .enjoyment-section_first_left, .enjoyment-section_first_right, .enjoyment-section_second_left, .enjoyment-section_second_right, .enjoyment-section_third_left, .enjoyment-section_third_right, .enjoyment-section_fourth_left, .enjoyment-section_fourth_right').each(function(){
+$('.enjoyment-section_main, .enjoyment-section_sub, .enjoyment-section_first_left, .enjoyment-section_first_right, .enjoyment-section_second_left, .enjoyment-section_second_right, .enjoyment-section_third_left, .enjoyment-section_third_right, .enjoyment-section_fourth_left, .enjoyment-section_fourth_right').each(function(){
   var targetPosition = $(this).offset().top;
   if(topWindow > targetPosition - windowHeight + 100){
-  $(this).addClass("second-section_fadeInDown");
+  $(this).addClass("enjoyment-section_fadeInDown");
   }
 });
 });
@@ -92,10 +92,10 @@ $(document).ready(function() {
   $('.trigger').click(function() {
     var value = $(this).attr('data-filter');
     if (value == 'all') {
-      $('.filter').show('1000');
+      $('.filter').show('2000');
     } else {
-      $('.filter').not('.'+value).hide('1000');
-      $('.filter').filter('.'+value).show('1000');
+      $('.filter').not('.'+value).hide('2000');
+      $('.filter').filter('.'+value).show('2000');
     }
     $(document).on('click', '.trigger', function () {
       $('.trigger').removeClass('active');
@@ -107,17 +107,5 @@ $(document).ready(function() {
 // PICTURE SECTION END
 
 // SPOT SECTION START
-
-$('.spot-section_main, .spot-section_sub, .spot-section_first_left, .spot-section_first_right, .spot-section_second_left, .spot-section_second_right, .spot-section_third_left, .spot-section_third_right, .spot-section_fourth_left, .spot-section_fourth_right, .spot-section_fifth_left, .spot-section_fifth_right, .spot-section_six_left, .spot-section_six_right').css('visibility','hidden');
-$(window).scroll(function(){
-var windowHeight = $(window).height(),
-    topWindow = $(window).scrollTop();
-$('.spot-section_main, .spot-section_sub, .spot-section_first_left, .spot-section_first_right, .spot-section_second_left, .spot-section_second_right, .spot-section_third_left, .spot-section_third_right, .spot-section_fourth_left, .spot-section_fourth_right, .spot-section_fifth_left, .spot-section_fifth_right, .spot-section_six_left, .spot-section_six_right').each(function(){
-  var targetPosition = $(this).offset().top;
-  if(topWindow > targetPosition - windowHeight + 100){
-  $(this).addClass("spot-section_fadeInDown");
-  }
-});
-});
 
 // SPOT SECTIONEND
