811757b8d25ea2c4fa797f57d1a321774dbb19fe
岩崎 和騎 <kazuki-iwasaki@iwasakinoMacBook-Pro.local>
2021-02-11 22:51:05 +0900

    ハンバーガーメニューの上部表示の途中です

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 // header fixed
 
-$('.burger-btn').on('click',function(){//.btn_triggerをクリックすると
-  $('.burger-btn').toggleClass('close');//.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
-  $('.nav-wrapper').fadeToggle(500);//.nav-wrapperが0.5秒でフェードイン(メニューのフェードイン)
-  $('body').toggleClass('noscroll');//bodyにnoscrollクラスを付与(スクロールを固定)
+$('.burger-btn').on('click',function(){
+  $('.burger-btn').toggleClass('close');
+  $('.nav-wrapper').fadeToggle(500);
+  $('body').toggleClass('noscroll');
  });
 
 // header fixed
