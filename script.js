770953da6f8491dfe72712ff607c1bae34e2378f
岩崎 和騎 <kazuki-iwasaki@iwasakinoMacBook-Pro.local>
2021-02-11 20:31:30 +0900

    ハンバーガーメニューの実装の途中までをpushします

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 
 // header fixed
 
-
+$('.burger-btn').on('click',function(){//.btn_triggerをクリックすると
+  $('.burger-btn').toggleClass('close');//.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
+  $('.nav-wrapper').fadeToggle(500);//.nav-wrapperが0.5秒でフェードイン(メニューのフェードイン)
+  $('body').toggleClass('noscroll');//bodyにnoscrollクラスを付与(スクロールを固定)
+ });
 
 // header fixed
 
