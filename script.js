6dc018ce355cfeb9ffc9db981e4834f5b0395e83
岩崎 和騎 <kazuki-iwasaki@iwasakinoMacBook-Pro.local>
2021-03-07 00:15:28 +0900

    完成しました

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 'use strict'
 
+// PageTopボタン
+
+$(function() {
+  var pagetop = $('.pagetop');
+  pagetop.hide();
+
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500);
+      return false;
+  });
+});
+
+// PageTopボタン
+
 // header fixed
 
 $('.burger-btn').on('click',function(){
@@ -22,40 +45,6 @@ window.addEventListener("scroll", function () {
 
 // header change-color PC
 
-
-
-// TOP PAGE START
-
-//   $(document).ready(function () {
-//     $("#topBtn").hide();　//ボタンを非表示にする
-//     $(window).on("scroll", function () {
-//         if ($(this).scrollTop() > 785) { //ページの上から100pxスクロールした時
-//             $("#topBtn").fadeIn("fast"); //ボタンがフェードインする
-//         } else {
-//             $("#topBtn").fadeOut("fast");　//ボタンがフェードアウトする
-//         }
-//         scrollHeight = $(document).height(); //ドキュメントの高さ 
-//         scrollPosition = $(window).height() + $(window).scrollTop(); //現在地 
-//         footHeight = $("footer").innerHeight(); //止めたい位置の高さ(今回はfooter)
-//         if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在地の差がfooterの高さ以下の時
-//             $("#topBtn").css({
-//                 "position": "absolute", //pisitionをabsoluteに変更
-//             });
-//         } else { //それ以外の場合は
-//             $("#topBtn").css({
-//                 "position": "fixed", //固定表示
-//             });
-//         }
-//     });
-
-// //スムーススクロール設定
-//     $('#topBtn').click(function () {
-//         $('body,html').animate({
-//             scrollTop: 0
-//         }, 800);//スムーススクロールの速度
-//         return false;
-//     });
-// });// TOP PAGE END
 
 // SECOND=SECTION START
 
